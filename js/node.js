cd6e28c352c6ba5313f60ea2ef9de7b4be94737d
Johannes <johannes.dahlgren@yooba.com>
2014-06-03 14:45:35 +0200

    added comments to node.js

--- js/node.js
+++ js/node.js
@@ -1,5 +1,8 @@
 uniqueID = 0;
 
+/*
+* The parent class Node with default properties that both files and folders will have
+*/
 function Node(name, type) {
 	this.name = name;
 	this.type = type;
@@ -10,18 +13,33 @@ function Node(name, type) {
 }
 
 
-
+/*
+* Class Folder calls node constructor
+* Folders have a children array
+*/
 function Folder(name){
 	Node.call(this, name, 'Folder');
 	this.children = [];
 };
+
+/*
+* A folder is a subclass to Node
+*/
 Folder.prototype = new Node();
+
+/*
+* Returns true if this folder has any children, otherwise false
+*/
 Folder.prototype.hasChildren = function(){
 	if (this.children.length > 0) {
 		return true;
 	}
 	return false;
 }
+
+/*
+* Adds a node to the children array of this folder
+*/
 Folder.prototype.add = function(node){
 	if(node.Parent === this){
 		return;//Already child of this folder
@@ -29,6 +47,10 @@ Folder.prototype.add = function(node){
 	this.children.push(node);
 	node.Parent = this;
 }
+
+/*
+* Removes a node from this folders childrens array
+*/
 Folder.prototype.remove = function(node){
 	if(node.Parent !== this){
 		return; //Not a child here
@@ -41,6 +63,9 @@ Folder.prototype.remove = function(node){
 	node.Parent = null;
 }
 
+/*
+* Class File, calls Nodes constructor
+*/
 function File(name, type){
 	Node.call(this, name, type);
 	this.size = null;
@@ -49,4 +74,8 @@ function File(name, type){
 	this.isImg = false;
 	this.thumb = null;
 };
+
+/*
+* File is a subclass of Node
+*/
 File.prototype = new Node();
